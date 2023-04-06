af905a96c03754ba325416bb9092753ed80d98b8
김민서 <gimminseonam722@gmail.com>
2023-04-06 05:13:45 +0000

    나의 c언어 프로그래밍5

---
+++ 49.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int num1 = 16;
+	float num2 = 3.14;
+	char nunja = 'm';
+	char munjas[5] = "ansan";
+	printf("%d\n",num1);
+	printf("%f\n", num2);
+	printf("%c\n",munja);
+	printf("%s\n",munjas);
+	
+	
+  
+	return 0;
+
+}	
