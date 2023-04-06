a7824aa4dc945fed27e555f90fad7389b4aebab0
김민서 <gimminseonam722@gmail.com>
2023-04-06 05:13:15 +0000

    나의 c언어 프로그래밍4

---
+++ 47b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+   int num1 =16;
+   int num2;
+   int sum=0; 
+   num2 = 61;
+   sum = num1 + num2;
+   sub= num1 - num2;
+   sum= num1 * num2;
+   div= num1 / num2;
+   mul= num1 - num2; 
+   printf("%d + %d = %d\n",num1, num2, sum);
+   printf("%d - %d = %d\n",num1, num2, sub);
+   printf("%d * %d = %d\n",num1, num2, div);
+   printf("%d - %d = %d\n",num1, num2, mul);
+	return 0;
+}
+	
