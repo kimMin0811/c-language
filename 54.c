7ce5dda3a9830d53671a58728e5f05a3dcf02b6f
김민서 <gimminseonam722@gmail.com>
2023-04-13 06:24:17 +0000

    (54),(55),(56),(58)

---
+++ 54.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ /*
+   주석문
+   주석문
+   */
+int main(){
+	
+	int a, a1, a2;// 변수명은 숫자가 앞에 올수 없다.
+	int A, cat, cat, cAt, caT;
+	int _a,__a, ___;
+	int if;//예약어 변수명을 사용할 수 없다.
+	
+	return 0;
+}	
